3337e7ee90f80cf51558b0dcf6d5a699a19bf3d4
pmcoding01 <pmcoding01.com@gmail.com>
2026-09-12 10:47:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+const SUPABASE_URL = "https://ljwgoxaitekuztapbevs.supabase.co"; // เช่น https://xxxx.supabase.co
+const SUPABASE_ANON_KEY = "sb_publishable_1KZqTZtVb_JEo3mAKgQxMw_DJ7ILywp"; // API Key (Anon)
